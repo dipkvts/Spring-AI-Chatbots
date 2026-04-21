533a11a4d02bf8dabad62082fc59ab1388d2727b
deepak <dipkvts@gmail.com>
2026-04-21 19:34:28 +0530

    Created context memory for OpenAI LLM

--- src/main/java/com/vats/deepak/ai/web/api/ChatbotController.java
+++ src/main/java/com/vats/deepak/ai/web/api/ChatbotController.java
@@ -3,11 +3,18 @@ package com.vats.deepak.ai.web.api;
 import com.vats.deepak.ai.beans.ChatbotRequest;
 import com.vats.deepak.ai.beans.ChatbotResponse;
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.messages.AssistantMessage;
+import org.springframework.ai.chat.messages.Message;
+import org.springframework.ai.chat.messages.SystemMessage;
+import org.springframework.ai.chat.messages.UserMessage;
 import org.springframework.ai.chat.prompt.ChatOptions;
 import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class ChatbotController {
@@ -34,11 +41,20 @@ public class ChatbotController {
 //                .call() //uses default model (gpt 4.0 mini), if not configured / specified
 //                .content(); //returns response, Actual AI provider api gets called at this moment
 
+        List<Message> msgList = new ArrayList<>();
+        msgList.add(new SystemMessage("You are my personal assistant."));
+        msgList.add(new UserMessage(request.question()));
+        msgList.add(new AssistantMessage("Welcome Mr. Deepak Vats. Nice to meet you."));
+        msgList.add(new UserMessage("Do you remember me ?"));
+
+        Prompt msgPrompt = new Prompt(msgList);
+
         String answer = chatClient
+                //.prompt(request.question())
                 //.prompt(prompt)
-                .prompt(request.question())
+                .prompt(msgPrompt)
                 .call() //uses default model (gpt 4.0 mini), if not configured / specified
-                .chatResponse()
+                .chatResponse() //Adding metadata by using ChatRssponse
                 .getResult()
                 .getOutput()
                 .getText();
